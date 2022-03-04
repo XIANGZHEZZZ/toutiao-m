0f5f5ba40f0cd3f4de124afb9782c1bcb08e705d
項哲 <593785042@qq.com>
2022-03-04 21:49:59 +0800

    项目初始化引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     123
+    <i class="toutiao toutiao-wode"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,21 @@ import store from './store'
 // 加载全局样式
 import './styles/index.less'
 
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+// // 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
+
+// 单个组件映入
+// import { Button, Cell, CellGroup } from 'vant'
+// Vue.use( Button, Cell )报错
+// Vue.use(Button)
+// Vue.use(Cell)
+// Vue.use(CellGroup)
+
 Vue.config.productionTip = false
 
 new Vue({
